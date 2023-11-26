49dcdddbc61596a2b2d4b14de6b7b2b577f90d71
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2023-11-26 18:53:02 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       String S = input.next();
+
+
+        System.out.println(S.length());
+
+
+    }
+
+
+}
+
