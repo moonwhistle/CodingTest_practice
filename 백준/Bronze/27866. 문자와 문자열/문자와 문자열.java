4801db9cf4ba406e90400450a56665a085adde1a
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2023-11-26 18:50:35 +0900

    [Bronze V] Title: 문자와 문자열, Time: 224 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       String S = input.next();
+       int i = input.nextInt();
+
+        System.out.println(S.substring(i-1,i));
+
+
+    }
+
+
+}
+
