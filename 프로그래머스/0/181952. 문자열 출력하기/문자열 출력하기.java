581627756b5b2c9edd2e2fc7f637730ebeece1b1
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-06-21 16:17:52 +0900

    [level 0] Title: 문자열 출력하기, Time: 162.27 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,8 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        if(a.length()<0 && a.length()>1000000){
-            a = sc.next();
+         if(a.length()<0 && a.length()>1000000){
+            throw new RuntimeException("예외");
         }
         System.out.println(a);
     }
