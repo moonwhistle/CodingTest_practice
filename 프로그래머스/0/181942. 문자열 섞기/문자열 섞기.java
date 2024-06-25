0d46ae9778a558a2acda3db4b3d79d1997a1e197
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-06-25 16:42:08 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder answer = new StringBuilder();
+         StringBuilder answer = new StringBuilder();
         char[] str11 = str1.toCharArray();
         char[] str22 = str2.toCharArray();
 
-        int i= 0;
-        for(char str111: str11){
-            answer.append(str111);
+        for(int i=0; i< str11.length; i++){
+            answer.append(str11[i]);
             answer.append(str22[i]);
-            i++;
         }
 
         return answer.toString();
