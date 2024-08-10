54b8f82787c15901d8e1db26370c726b7d9086e7
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-08-10 15:57:59 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % 2 == 0) {
+                if (i % 2 == 0) {
+                    evenSum += i * i;
+                }
+            }
+            if (i % 2 != 0) {
+                oddSum += i;
+            }
+        }
+        return n % 2 == 0 ? evenSum : oddSum;
+    }
+}
