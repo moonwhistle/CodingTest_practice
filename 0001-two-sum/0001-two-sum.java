f34295a5585c37792eba22168a44c7a7397573ec
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-08-18 14:58:11 +0900

    Time: 44 ms (40.22%), Space: 44.7 MB (60.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = {};
-        for(int i=0;i<nums.length-1; i++){
-            for(int k=i+1 ; k<nums.length; k++){
-                if(nums[i] + nums[k] == target){
-                    answer = new int[]{i, k};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                if (nums[i] + nums[k] == target) {
+                    return new int[]{i, k};
                 }
             }
         }
-        return answer;
+        return null;
     }
 }
