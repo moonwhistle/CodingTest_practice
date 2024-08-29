c4dd82cad534198555a5fca4e946ca807105314a
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-08-30 03:04:32 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 9.62 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int count = 0;
+        for (int[] query : queries) {
+            List<Integer> minList = new ArrayList<>();
+            for (int i = query[0]; i < query[1] + 1; i++) {
+                if (arr[i] > query[2]) {
+                    minList.add(arr[i]);
+                }
+            }
+            if(minList.isEmpty()){
+                answer[count] = -1;
+            } else {
+                answer[count] = Collections.min(minList);
+            }
+            count++;
+        }
+        return answer;
+    }
+}
