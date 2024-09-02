275e201e1aecffa21e1ec899c3de27b94e808897
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-02 10:07:43 +0900

    [level 0] Title: 배열 만들기 4, Time: 31.01 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while (i != arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) >= arr[i]) {
+                stk.remove(stk.get(stk.size() - 1));
+            }
+        }
+        return stk.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
