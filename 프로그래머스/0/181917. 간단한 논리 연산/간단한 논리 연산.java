157c2f75473c2d46a4f0d8407334a9fe559ff9d4
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-03 22:21:36 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,25 +1,5 @@
 class Solution {
     public static boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        boolean firstCalculation = true;
-        boolean secondCalculation = true;
-        if (x1 == x2) {
-            if (!x1) {
-                firstCalculation = false;
-            }
-        }
-        if (x3 == x4) {
-            if (!x3) {
-                secondCalculation = false;
-            }
-        }
-        if (firstCalculation == secondCalculation) {
-            if (!firstCalculation) {
-                answer = false;
-            }
-        } else {
-            answer = false;
-        }
-        return answer;
+        return (x1 || x2) && (x3 || x4);
     }
 }
