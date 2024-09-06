bbac768a0f8bbf94da567c5f3e221739d8b14411
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-06 23:57:25 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        int k = 1;
-        String[] arr = new String[my_string.length()];
-        for (int i = 0; i < my_string.length(); i++) {
-            arr[i] = my_string.substring(0, k);
-            k++;
+        if (my_string.length() < is_prefix.length()) {
+            return answer;
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(is_prefix)) {
-                answer = 1;
-            }
+        String prefix = my_string.substring(0, is_prefix.length());
+        if(prefix.equals(is_prefix)){
+            answer = 1;
         }
         return answer;
     }
