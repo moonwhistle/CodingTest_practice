50cc2938b4c45e5773173adf3e60038e97827e53
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-08 21:59:33 +0900

    [level 0] Title: 접미사 배열, Time: 0.83 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
-        List<String> stringList = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
         for (int i = my_string.length() - 1; i >= 0; i--) {
-            stringList.add(my_string.substring(i));
+            answer.add(my_string.substring(i));
         }
-        Collections.sort(stringList);
-        int count = 0;
-        for (String string : stringList) {
-            answer[count] = string;
-            count++;
-        }
-        return answer;
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
     }
 }
