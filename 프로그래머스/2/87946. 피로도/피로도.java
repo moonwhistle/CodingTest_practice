47d7c0b551693dc546ac5a2a7c086889038a474b
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-01-16 16:45:23 +0900

    [level 2] Title: 피로도, Time: 4.39 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    static boolean[] visited;
+    static int max = 0;
+
+    public static int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, max);
+        return max;
+    }
+
+    private static void dfs(int k, int[][] dungeons, int count) {
+        max = Math.max(max , count);
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
