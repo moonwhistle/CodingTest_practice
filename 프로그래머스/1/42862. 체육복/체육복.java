4bc8f7286b2bc936787bc60d486f9b6f68c1b817
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-01-18 02:53:37 +0900

    [level 1] Title: 체육복, Time: 0.78 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,34 +1,43 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 class Solution {
 
     public static int solution(int n, int[] lost, int[] reserve) {
-       // 도난당한 학생과 여벌 체육복을 가진 학생을 각각 Set으로 저장
-        Set<Integer> lostSet = new HashSet<>();
-        Set<Integer> reserveSet = new HashSet<>();
-        for (int l : lost) lostSet.add(l);
-        for (int r : reserve) reserveSet.add(r);
+        int answer = 0;
+        //세팅
+        int[] student = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            student[i] = 1;
+        }
 
-        // 교집합(여벌이 있으면서 도난당한 학생) 처리
-        Set<Integer> intersection = new HashSet<>(lostSet);
-        intersection.retainAll(reserveSet); // 교집합 계산
-        lostSet.removeAll(intersection);   // 도난당한 리스트에서 제거
-        reserveSet.removeAll(intersection);// 여벌 리스트에서 제거
+        //여분
+        for (int i : reserve) {
+            student[i] = 2;
+        }
 
-        // 체육복 빌려주기
-        for (int r : reserveSet) {
-            if (lostSet.contains(r - 1)) {   // 앞 번호 학생에게 빌려줌
-                lostSet.remove(r - 1);
-            } else if (lostSet.contains(r + 1)) { // 뒷 번호 학생에게 빌려줌
-                lostSet.remove(r + 1);
+        //잃어버림
+        for (int i : lost) {
+            student[i] -= 1;
+        }
+
+        //빌려주기
+        for (int i = 1; i <= n; i++) {
+            if (student[i] == 2) {
+                if (i > 1 && student[i - 1] == 0) {
+                    student[i] -= 1;
+                    student[i - 1] += 1;
+                } else if (i < n && student[i + 1] == 0) {
+                    student[i] -= 1;
+                    student[i + 1] += 1;
+                }
             }
         }
 
-        // 체육복을 가진 학생의 수 계산
-        return n - lostSet.size();
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println(student[i]);
+            if (student[i] > 0) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
