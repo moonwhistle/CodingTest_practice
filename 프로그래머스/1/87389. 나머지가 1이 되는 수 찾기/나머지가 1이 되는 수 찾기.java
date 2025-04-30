421408467c0b9ecb244f7340485538dbf69edddc
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-04-30 16:03:33 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.08 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        if (answer == 0) {
+            answer = n - 1;
+        }
+        return answer;
+    }
+}
