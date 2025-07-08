5a39b0719851c9ed2d9aa34a889bea64c7271c87
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-07-08 22:59:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1932 ms, Memory: 256620 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,53 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+
+    static int[] sum;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-       int N = input.nextInt();
-       int M = input.nextInt();
+        int N = input.nextInt();
+        int M = input.nextInt();
 
-       int[] arr = new int[N];
+        sum = new int[N + 1];
 
-       for(int i=0;i<N;i++) //수 입력
-       {
-           arr[i] = input.nextInt();
-       }
+        // sum 배열 만들기
+        for (int i = 1; i < N + 1; i++) {
+            if (i == 1) {
+                sum[i] = input.nextInt();
+                continue;
+            }
 
-       int[] sum = new int[N]; //arr합 담을 배열
+            sum[i] = sum[i - 1] + input.nextInt();
+        }
 
-        sum[0] = arr[0]; //첫 번째 값 지정
+        // i,j 입력받고 구간 합 출력하기
+        for (int c = 0; c < M; c++) {
+            // i, j 입력받기
+            int i = input.nextInt();
+            int j = input.nextInt();
 
-       int s = sum[0];
-
-       for(int i=1;i<N;i++) //0번째 정해줬으니 1부터, 합 구해줌
-       {
-           s += arr[i];
-           sum[i] = s;
-       }
-
-       for(int k=0;k<M;k++)
-       {
-           int i = input.nextInt();
-           int j = input.nextInt();
-
-           if(i==j) //같은 수 들어올 때
-           {
-               System.out.println(arr[j-1]); //배열값 출력
-           }
-           else if(i==1) //1일때 index = 0
-           {
-               System.out.println(sum[j-1]); //전체 합
-           }
-           else{ //일반적인 경우
-               System.out.println(sum[j-1]-sum[i-2]);
-           }
-
-
-
-
-       }
-
-
+            // 구간 합 출력하기
+            System.out.println(sum[j] - sum[i - 1]);
+        }
     }
 }
