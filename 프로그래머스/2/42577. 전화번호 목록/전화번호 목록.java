4739826bb26d6f190d2dd878ee386d85e768fd2a
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-16 21:28:22 +0900

    [level 2] Title: 전화번호 목록, Time: 361.65 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,12 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 class Solution {
-
-    // input 배열 정렬 후, 리스트 값에 넣으며 비교
-
-    public static boolean solution(String[] phone_book) {
+    public boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
 
-        for(int i = 0; i<phone_book.length -1; i++) {
-            String first = phone_book[i];
-            String next = phone_book[i+1];
-            if(next.startsWith(first)) {
+        for(int i = 0; i<phone_book.length - 1; i++) {
+            if(phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
             }
         }
