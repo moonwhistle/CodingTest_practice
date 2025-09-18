f7c2ca260c473072a8e6761d99edf5c845d9ea12
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-18 22:58:48 +0900

    [Silver III] Title: N과 M (1), Time: 1716 ms, Memory: 60772 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        backTracking(M, new ArrayList<>(), 1, N);
+    }
+
+    private static void backTracking(int size, List<Integer> nums, int start, int last) {
+        if(size == nums.size()) {
+            for(int i : nums) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = start; i<last + 1; i++) {
+            if(nums.contains(i)) {
+                continue;
+            }
+            nums.add(i);
+            backTracking(size, nums, start, last);
+            nums.remove(nums.size() - 1);
+        }
+    }
+}
