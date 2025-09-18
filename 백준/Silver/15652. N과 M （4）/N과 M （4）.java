cba11b4f718d7816bfe295c010dd99d825ca40f6
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-18 23:22:30 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 20424 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        backTracking(M, new ArrayList<>(), N);
+        System.out.print(sb.toString());
+    }
+
+    private static void backTracking(int size, List<Integer> nums, int last) {
+        if (size == nums.size()) {
+            for (int i : nums) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i < last + 1; i++) {
+            if(!nums.isEmpty() && nums.get(nums.size() -1) > i) {
+                continue;
+            }
+            nums.add(i);
+            backTracking(size, nums, last);
+            nums.remove(nums.size() - 1);
+        }
+    }
+}
