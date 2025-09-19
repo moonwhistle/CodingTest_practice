dbe66744ee137db910c4268d578b3a6ec53f6810
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-19 09:45:29 +0900

    [level 1] Title: 모의고사, Time: 3.82 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,43 +2,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    
+
+    static int[] first = new int[]{1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+    static int[] second = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+    static int[] third = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
     public int[] solution(int[] answers) {
-        List<Integer> answer = new ArrayList<>();
-        int[] one = new int[]{1, 2, 3, 4, 5};
-        int[] two = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        if (answers.length == 0) {
+            return new int[]{1, 2, 3};
+        }
 
-        int oneCount = 0;
-        int twoCount = 0;
-        int threeCount = 0;
-
-        for(int i = 0; i<answers.length ; i ++) {
-            if(answers[i] == one[i % one.length]) {
-                oneCount++;
+        int idx = 0;
+        int[] count = new int[3];
+        while (idx != answers.length) {
+            if (first[idx % 10] == answers[idx]) {
+                count[0]++;
             }
-            if(answers[i] == two[i % two.length]) {
-                twoCount++;
+            if (second[idx % 8] == answers[idx]) {
+                count[1]++;
             }
-            if(answers[i] == three[i % three.length]) {
-                threeCount++;
+            if (third[idx % 10] == answers[idx]) {
+                count[2]++;
+            }
+
+            idx++;
+        }
+
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
+        List<Integer> maxMember = new ArrayList<>();
+        for (int i = 0; i < count.length; i++) {
+            if (max == count[i]) {
+                maxMember.add(i + 1);
             }
         }
-
-        int oneBetweenTwoMax = Math.max(oneCount, twoCount);
-        int max = Math.max(oneBetweenTwoMax, threeCount);
-
-        if(max == oneCount) {
-            answer.add(1);
-        }
-        if(max == twoCount) {
-            answer.add(2);
-        }
-        if(max == threeCount) {
-            answer.add(3);
-        }
-
-        return answer.stream()
+        return maxMember.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
