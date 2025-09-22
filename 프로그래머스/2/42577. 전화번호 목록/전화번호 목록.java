fffad50e46be124bf5834fd781f38f02d5283147
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-22 10:31:12 +0900

    [level 2] Title: 전화번호 목록, Time: 297.65 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,8 +5,8 @@ class Solution {
         boolean answer = true;
         Arrays.sort(phone_book);
 
-        for(int i = 0; i<phone_book.length - 1; i++) {
-            if(phone_book[i + 1].startsWith(phone_book[i])) {
+        for(int i = 1; i<phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) {
                 return false;
             }
         }
