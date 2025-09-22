251e1ec71c36b339b48903526a768be080eb7cf1
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-22 13:39:24 +0900

    [level 1] Title: 모의고사, Time: 4.11 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -8,34 +8,36 @@ class Solution {
     static int[] third = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
     public int[] solution(int[] answers) {
-        if (answers.length == 0) {
-            return new int[]{1, 2, 3};
-        }
-
+        int[] count = new int[4];
         int idx = 0;
-        int[] count = new int[3];
-        while (idx != answers.length) {
-            if (first[idx % 10] == answers[idx]) {
-                count[0]++;
-            }
-            if (second[idx % 8] == answers[idx]) {
+
+        while(idx != answers.length) {
+            if(answers[idx] == first[idx % 10]) {
                 count[1]++;
             }
-            if (third[idx % 10] == answers[idx]) {
+            if(answers[idx] == second[idx % 8]) {
                 count[2]++;
             }
-
+            if(answers[idx] == third[idx % 10]) {
+                count[3]++;
+            }
+            
             idx++;
         }
 
-        int max = Math.max(count[0], Math.max(count[1], count[2]));
-        List<Integer> maxMember = new ArrayList<>();
-        for (int i = 0; i < count.length; i++) {
-            if (max == count[i]) {
-                maxMember.add(i + 1);
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i<4; i++) {
+            max = Math.max(count[i], max);
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 1; i<4; i++) {
+            if(count[i] == max) {
+                answer.add(i);
             }
         }
-        return maxMember.stream()
+        
+        return answer.stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
