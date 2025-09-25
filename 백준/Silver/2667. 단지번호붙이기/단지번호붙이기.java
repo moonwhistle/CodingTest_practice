d9047d0835bfffd1b3dde6b338c193b7335d868e
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-09-25 13:17:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 188 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,67 +3,64 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-//1. map 을 만든다
-//2. 좌표를 이용한다
-//3. 끊어지고 새로 시작하는 부분끼리 나눈다
 public class Main {
 
     static int[][] map;
-    static boolean[][] visited;
-    static int count = 0;
-    static List<Integer> house = new ArrayList<>();
-    //좌표 세팅
     static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visited;
+    static List<Integer> box = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // setting
         int N = input.nextInt();
         map = new int[N][N];
         visited = new boolean[N][N];
 
-        // map 세팅
         for (int i = 0; i < N; i++) {
-            String numbers = input.next();
-            for (int k = 0; k < N; k++) {
-                map[i][k] = numbers.charAt(k) - '0';
+            String line = input.next();
+
+            int j = 0;
+            for (char lineElement : line.toCharArray()) {
+                map[i][j] = lineElement - '0';
+                j++;
             }
         }
 
         for (int i = 0; i < N; i++) {
-            for (int k = 0; k < N; k++) {
-                //아직 방문하지 않았다면
-                if (!visited[i][k] && map[i][k] == 1) {
-                    count++;
-                    int count = dfs(i, k, N, 0);
-                    house.add(count);
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    int cnt = dfs(j, i);
+                    box.add(cnt);
                 }
             }
         }
 
-        Collections.sort(house);
+        Collections.sort(box);
 
-        System.out.println(count);
-        for(int i : house) {
+        System.out.println(box.size());
+        for(int i : box) {
             System.out.println(i);
         }
     }
 
-    private static int dfs(int x, int y, int N, int count) {
-        visited[x][y] = true;
-        count++;
+    private static int dfs(int startX, int startY) {
+        visited[startY][startX] = true;
+        int cnt = 1;
 
         for (int i = 0; i < 4; i++) {
-            int xx = x + dx[i];
-            int yy = y + dy[i];
-            if (xx >= 0 && yy >= 0 && xx < N && yy < N) {
-                if (!visited[xx][yy] && map[xx][yy] == 1) {
-                    count = dfs(xx, yy, N, count);
+            int x = dx[i] + startX;
+            int y = dy[i] + startY;
+
+            if (x >= 0 && y >= 0 && x < map[0].length && y < map.length) {
+                if (!visited[y][x] && map[y][x] == 1) {
+                    cnt += dfs(x, y);
                 }
             }
         }
 
-        return count;
+        return cnt;
     }
 }
