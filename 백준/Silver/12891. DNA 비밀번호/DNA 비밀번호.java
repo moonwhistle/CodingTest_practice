a9194c4de2a8e62e6b2f663bf3c6e66ff55691b1
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-10-10 11:43:01 +0900

    [Silver II] Title: DNA 비밀번호, Time: 436 ms, Memory: 29764 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,76 +2,73 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int passwordCount = 0;
-    static int A;
-    static int C;
-    static int G;
-    static int T;
+    private static int passwordCount = 0;
 
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
 
+        // setting
         int S = input.nextInt();
         int P = input.nextInt();
-        String given = input.next();
-        char[] DNA = given.toCharArray();
 
-        A = input.nextInt();
-        C = input.nextInt();
-        G = input.nextInt();
-        T = input.nextInt();
+        String password = input.next();
+        int[] count = new int[4];
 
-        //세팅
-        int[] setting = new int[4];
-        for (int i = 0; i < P; i++) {
-            addDNA(setting, DNA[i]);
+        for (int i = 0; i < 4; i++) {
+            count[i] = input.nextInt();
         }
-        countPassword(setting);
 
-        for (int i = P; i < S; i++) {
-            addDNA(setting, DNA[i]);
-            removeDNA(setting, DNA[i-P]);
+        // 처음 주어진 개수 비교
+        int[] currentCount = new int[4];
 
-            countPassword(setting);
+        for(int i = 0 ; i<P; i++) {
+            currentCount[getIndex(password.charAt(i))]++;
+        }
+
+        countPassword(count, currentCount);
+
+        // 이동하며 비교
+        for(int i = P; i< password.length(); i++) {
+            char first = password.charAt(i - P);
+            char last = password.charAt(i);
+
+            currentCount[getIndex(first)]--;
+            currentCount[getIndex(last)]++;
+
+            countPassword(count, currentCount);
         }
 
         System.out.println(passwordCount);
     }
 
-    private static void addDNA(int[] setting, char DNA) {
-        if (DNA == 'A') {
-            setting[0]++;
-        }
-        if (DNA == 'C') {
-            setting[1]++;
-        }
-        if (DNA == 'G') {
-            setting[2]++;
-        }
-        if (DNA == 'T') {
-            setting[3]++;
-        }
-    }
+    private static void countPassword(int[] count, int[] currentCount) {
+        boolean isPassword = true;
 
-    private static void removeDNA(int[] setting, char DNA) {
-        if (DNA == 'A') {
-            setting[0]--;
+        for (int i = 0; i < 4; i++) {
+            if (count[i] > currentCount[i]) {
+                isPassword = false;
+            }
         }
-        if (DNA == 'C') {
-            setting[1]--;
-        }
-        if (DNA == 'G') {
-            setting[2]--;
-        }
-        if (DNA == 'T') {
-            setting[3]--;
-        }
-    }
 
-    private static void countPassword(int[] setting) {
-        if (setting[0] >= A && setting[1] >= C && setting[2] >= G && setting[3] >= T) {
+        if (isPassword) {
             passwordCount++;
         }
+    }
+
+    private static int getIndex(char c) {
+        if (c == 'A') {
+            return 0;
+        }
+        if (c == 'C') {
+            return 1;
+        }
+        if (c == 'G') {
+            return 2;
+        }
+        if (c == 'T') {
+            return 3;
+        }
+        
+        return -1;
     }
 }
