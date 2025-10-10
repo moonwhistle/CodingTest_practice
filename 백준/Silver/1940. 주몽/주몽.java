55380ef99de79df5bde7e0620f3203ece68412d5
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-10-10 09:46:44 +0900

    [Silver IV] Title: 주몽, Time: 384 ms, Memory: 34412 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,44 +1,39 @@
-import javax.crypto.spec.PSource;
-import java.util.*;
-
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int N = input.nextInt();
         int M = input.nextInt();
 
-        int[] mat = new int[N];
+        int[] elements = new int[N];
 
-        for(int i=0;i<N;i++)
-        {
-            mat[i] = input.nextInt();
+        for (int i = 0; i < N; i++) {
+            elements[i] = input.nextInt();
         }
 
+        Arrays.sort(elements);
+
+        int start = 0;
+        int last = N - 1;
         int cnt = 0;
 
-        for(int i=0;i<N-1;i++)
-        {
-            int sum = mat[i]; //기준
+        while (start < last) {
+            int sum = elements[start] + elements[last];
 
-            for(int j=i+1;j<N;j++)
-            {
-                sum += mat[j];
-                
-                if(sum == M)
-                {
-                    cnt++;
-                }
-                sum -= mat[j];
+            if(sum > M) {
+                last--;
+            } else if(sum < M) {
+                start++;
+            } else {
+                cnt++;
+                start++;
             }
         }
 
         System.out.println(cnt);
-
-
-
-
     }
 }
