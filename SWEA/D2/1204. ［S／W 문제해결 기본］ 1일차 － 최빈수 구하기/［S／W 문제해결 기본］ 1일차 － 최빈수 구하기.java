9bdc6d42bc60ecf00973559284d93b7612a21754
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-10 10:14:08 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 170 ms, Memory: 41,220 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,90 +1,62 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.io.FileInputStream;
 import java.util.TreeMap;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution {
-    public static void main(String args[]) throws Exception {
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-        //System.setIn(new FileInputStream("res/input.txt"));
+public class Solution {
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int T;
-        T = input.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int[] students = new int[1001];
-            int max = Integer.MIN_VALUE;
-            int textCaseNum = input.nextInt();
+        int T = input.nextInt();
 
-            for (int i = 0; i < 1000; i++) {
+        for (int i = 1; i <= T; i++) {
+            int test = input.nextInt();
+            Map<Integer, Number> counts = new HashMap<>();
+
+            for (int j = 0; j < 1000; j++) {
                 int num = input.nextInt();
-                students[num]++;
-
-                if (students[num] > max) {
-                    max = students[num];
-                }
+                counts.putIfAbsent(num, new Number(num));
+                Number number = counts.get(num);
+                number.add();
             }
 
-            List<Integer> maxStudents = new ArrayList<>();
+            List<Number> numbers = new ArrayList<>(counts.values());
+            Collections.sort(numbers);
 
-            for (int i = 1; i < students.length; i++) {
-                if (students[i] == max) {
-                    maxStudents.add(i);
-                }
-            }
-
-            Collections.sort(maxStudents);
-
-            System.out.println("#" + textCaseNum + " " + maxStudents.get(maxStudents.size() - 1));
+            System.out.println("#" + test + " " + numbers.get(0).num());
         }
+    }
+}
+
+class Number implements Comparable<Number> {
+
+    private final int num;
+    private int count;
+
+    Number(int num) {
+        this.num = num;
+        this.count = 0;
+    }
+
+    @Override
+    public int compareTo(Number o) {
+        if (count == o.count) {
+            return o.num - this.num;
+        }
+
+        return o.count - this.count;
+    }
+
+    public void add() {
+        this.count++;
+    }
+
+    public int num() {
+        return num;
     }
 }
