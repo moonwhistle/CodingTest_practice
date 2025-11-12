9342fb17721f4614b64a012c0a5a407c1677cb15
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-12 10:27:26 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 99 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -5,21 +5,24 @@ public class Solution {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        for (int test = 0; test < 10; test++) {
-            int testNumber = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            int test = input.nextInt();
 
-            int number = input.nextInt();
+            int num = input.nextInt();
             int count = input.nextInt();
 
-            System.out.println("#" + testNumber + " " + btk(number, count, 0, 1));
+            int answer = btk(num, count, 0, 1);
+            System.out.println("#" + test + " " + answer);
         }
     }
 
-    private static int btk(int number, int targetCount, int count, int sum) {
-        if (count == targetCount) {
+    private static int btk(int num, int count, int nowCount, int sum) {
+        if (nowCount == count) {
             return sum;
         }
 
-        return btk(number, targetCount, count + 1, sum * number);
+        sum = btk(num, count, nowCount + 1, sum * num);
+
+        return sum;
     }
 }
