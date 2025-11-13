53804e40c1e97a62bcbd3305059c18cd20b28e1e
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-13 11:01:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.70 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,25 +1,18 @@
 import java.util.*;
 
 public class Solution {
-    public static int[] solution(int[] arr) {
-        int[] answer;
-        //세팅
-        Stack<Integer> stack = new Stack<>();
-        stack.add(arr[0]);
-        //조건에 맞추어 넣기
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        answer[0] = arr[0];
+
         for (int i = 1; i < arr.length; i++) {
-            int num = arr[i];
-            if (stack.peek() != num) {
-                stack.add(num);
+            if (answer[idx] != arr[i]) {
+                idx++;
+                answer[idx] = arr[i];
             }
         }
-        //배열 크기 선언
-        answer = new int[stack.size()];
-        //배열 크기에 맞추어 넣어주기
-        for (int i = stack.size() - 1; i >= 0; i--) {
-            answer[i] = stack.pop();
-        }
 
-        return answer;
+        return Arrays.copyOf(answer, idx + 1);
     }
 }
