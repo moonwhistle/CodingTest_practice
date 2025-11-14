d9e5159b580806956b9300599f56a4962fd30fd6
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-14 23:42:22 +0900

    [D2] Title: 파리 퇴치, Time: 114 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,41 +1,50 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
+
+    static int[][] map;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int T = input.nextInt();
 
-        for (int t = 1; t <= T; t++) {
+        for (int i = 1; i <= T; i++) {
             int N = input.nextInt();
             int M = input.nextInt();
+            map = new int[N][N];
 
-            int[][] map = new int[N][N];
+            for (int y = 0; y < N; y++) {
+                for (int x = 0; x < N; x++) {
+                    int num = input.nextInt();
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = input.nextInt();
+                    if (x == 0) {
+                        map[y][x] = num;
+                    } else {
+                        map[y][x] = num + map[y][x - 1];
+                    }
                 }
             }
 
             int max = Integer.MIN_VALUE;
 
-            for (int col = 0; col <= N - M; col++) {
-                for (int row = 0; row <= N - M; row++) {
+            for (int y = 0; y < N - M + 1; y++) {
+                for (int x = M - 1; x < N; x++) {
                     int sum = 0;
 
-                    for (int y = col; y < col + M; y++) {
-                        for (int x = row; x < row + M; x++) {
-                            sum += map[y][x];
+                    for (int yy = y; yy < y + M; yy++) {
+                        if (x == M - 1) {
+                            sum += map[yy][x];
+                        } else {
+                            sum += map[yy][x] - map[yy][x - M];
                         }
                     }
 
-                    max = Math.max(max, sum);
+                    max = Math.max(sum, max);
                 }
             }
 
-            System.out.println("#" + t + " " + max);
+            System.out.println("#" + i + " " + max);
         }
     }
 }
