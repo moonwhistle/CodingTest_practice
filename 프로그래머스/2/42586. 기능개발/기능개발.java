5fe69a03a88cc79eb48846382f9bd63a3c25c5a9
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-14 13:56:37 +0900

    [level 2] Title: 기능개발, Time: 3.29 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,52 +1,39 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Map;
+import java.util.List;
 import java.util.Queue;
-import java.util.TreeMap;
 
 class Solution {
 
-    // stack 에 값 넣어주고
-    // count 세줌
-    // (count * speed) + prog 가 100 넘는지 확인
-
-    static Queue<Integer> proSt = new LinkedList<>();
-    static Queue<Integer> spSt = new LinkedList<>();
+    static int count = 0;
 
     public int[] solution(int[] progresses, int[] speeds) {
-        setting(progresses, speeds);
-        Map<Integer, Integer> completeCount = new TreeMap<>();
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> progressQueue = new LinkedList<>();
+        Queue<Integer> speedQueue = new LinkedList<>();
 
-        int count = 1;
+        for (int i = 0; i < progresses.length; i++) {
+            progressQueue.add(progresses[i]);
+            speedQueue.add(speeds[i]);
+        }
 
-        while (!proSt.isEmpty()) {
-            if (proSt.peek() + count * spSt.peek() >= 100) {
-                completeCount.put(count, completeCount.getOrDefault(count, 0) + 1);
-                proSt.poll();
-                spSt.poll();
-            } else {
-                count++;
+        while (!progressQueue.isEmpty()) {
+            count++;
+            int doneCount = 0;
+
+            while (!progressQueue.isEmpty() && progressQueue.peek() + (speedQueue.peek() * count) >= 100) {
+                doneCount++;
+                progressQueue.poll();
+                speedQueue.poll();
+            }
+
+            if (doneCount != 0) {
+                answer.add(doneCount);
             }
         }
 
-        int[] answer = new int[completeCount.size()];
-        
-        int idx = 0;
-        for(int i : completeCount.values()) {
-            answer[idx] = i;
-            idx++;
-        }
-        
-        return answer;
-    }
-
-    private void setting(int[] progresses, int[] speeds) {
-        for (int i : progresses) {
-            proSt.add(i);
-        }
-
-        for (int i : speeds) {
-            spSt.add(i);
-        }
+        return answer.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
