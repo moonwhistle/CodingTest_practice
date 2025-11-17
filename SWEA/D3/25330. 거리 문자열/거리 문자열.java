b3274674ef7a2e65f785dc375f3cc7d9ed4b5ff8
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-17 11:08:00 +0900

    [D3] Title: 거리 문자열, Time: 104 ms, Memory: 27,904 KB -BaekjoonHub

--- SWEA/D3/25330. 거리 문자열/거리 문자열.java
+++ SWEA/D3/25330. 거리 문자열/거리 문자열.java
@@ -4,43 +4,40 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        int T = input.nextInt();
+		int testCase = input.nextInt();
 
-        for (int test = 1; test <= T; test++) {
-            String given = input.next();
-            Map<Character, Integer> number = new HashMap<>();
-            boolean isSatisfied = true;
+		for (int i = 1; i <= testCase; i++) {
+			String line = input.next();
+			Map<Character, Integer> existBox = new HashMap<>();
+			boolean isSatisfied = true;
 
-            for (int i = 0; i < given.length(); i++) {
-                char now = given.charAt(i);
-                number.put(now, number.getOrDefault(now, 0) + 1);
+			for (int j = 0; j < line.length(); j++) {
+				char now = line.charAt(j);
+				existBox.put(now, existBox.getOrDefault(now, 0) + 1);
 
-                if (number.get(now) < 2) {
-                    int last = now - '0' + 1 + i;
+				if (existBox.get(now) < 2) {
+					int nextIdx = j + 1 + (now - '0');
 
-                    if (last >= given.length()) {
-                        isSatisfied = false;
-                        break;
-                    }
+					if (nextIdx >= line.length() || now != line.charAt(nextIdx)) {
+						isSatisfied = false;
+						break;
+					}
+				} else if (existBox.get(now) == 2) {
+					continue;
+				} else {
+					isSatisfied = false;
+					break;
+				}
+			}
 
-                    if (given.charAt(last) != now) {
-                        isSatisfied = false;
-                        break;
-                    }
-                } else if (number.get(now) > 2) {
-                    isSatisfied = false;
-                    break;
-                }
-            }
-
-            if(isSatisfied) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
-        }
-    }
+			if (isSatisfied) {
+				System.out.println("yes");
+			} else {
+				System.out.println("no");
+			}
+		}
+	}
 }
