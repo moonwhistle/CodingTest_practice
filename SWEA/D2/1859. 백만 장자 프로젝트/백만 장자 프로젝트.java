fd2764346f71191362823dd8d14a5c083c74c997
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-20 13:53:41 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,790 ms, Memory: 131,108 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        int T = input.nextInt();
+		int T = input.nextInt();
 
-        for (int i = 1; i <= T; i++) {
-            int N = input.nextInt();
-            long[] nums = new long[N];
+		for (int i = 1; i <= T; i++) {
+			int N = input.nextInt();
+			int[] sells = new int[N];
 
-            for (int j = 0; j < N; j++) {
-                nums[j] = input.nextInt();
-            }
+			for (int j = 0; j < N; j++) {
+				sells[j] = input.nextInt();
+			}
 
-            int maxIdx = N - 1;
-            long sum = 0;
+			int end = N - 1;
+			long sum = 0;
 
-            for (int j = N - 2; j >= 0; j--) {
-                if (nums[maxIdx] < nums[j]) {
-                    maxIdx = j;
-                } else {
-                    sum += nums[maxIdx] - nums[j];
-                }
-            }
+			for (int j = N - 2; j >= 0; j--) {
+				int delimiter = sells[end];
+				int now = sells[j];
 
-            System.out.println("#" + i + " " + sum);
-        }
-    }
+				if (delimiter > now) {
+					sum += (delimiter - now);
+				} else {
+					end = j;
+				}
+			}
+
+			System.out.println("#" + i + " " + sum);
+		}
+	}
 }
