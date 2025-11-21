19b8c1e9ec88d33937fe3f83a79f67dba6791160
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-21 11:34:51 +0900

    [D3] Title: 레벨업, Time: 131 ms, Memory: 31,060 KB -BaekjoonHub

--- SWEA/D3/24524. 레벨업/레벨업.java
+++ SWEA/D3/24524. 레벨업/레벨업.java
@@ -1,36 +1,35 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        int T = input.nextInt();
+		int T = input.nextInt();
 
-        for (int test = 1; test <= T; test++) {
-            int N = input.nextInt();
-            int[] nums = new int[N];
+		for (int i = 1; i <= T; i++) {
+			int N = input.nextInt();
+			int[] nums = new int[N];
 
-            for (int i = 0; i < N; i++) {
-                nums[i] = input.nextInt();
-            }
+			for (int j = 0; j < N; j++) {
+				nums[j] = input.nextInt();
+			}
 
-            int sum = 0;
-            int min = Integer.MAX_VALUE;
-            for (int i = 1; i < N; i++) {
-                sum += Math.abs(nums[i] - nums[i - 1]);
-            }
+			int min = 0;
 
-            for (int i = 1; i < N - 1; i++) {
-                int front = Math.abs(nums[i] - nums[i - 1]);
-                int back = Math.abs(nums[i + 1] - nums[i]);
-                int between = Math.abs(nums[i + 1] - nums[i - 1]);
-                sum = sum - (front + back) + between;
-                min = Math.min(sum, min);
-                sum = sum + (front + back) - between;
-            }
+			for (int j = 1; j < N; j++) {
+				min += Math.abs(nums[j] - nums[j - 1]);
+			}
 
-            System.out.println(min);
-        }
-    }
+			int answer = Integer.MAX_VALUE;
+
+			for (int j = 1; j < N - 1; j++) {
+				int origin = Math.abs(nums[j] - nums[j - 1]) + Math.abs(nums[j + 1] - nums[j]);
+				int removed = Math.abs(nums[j + 1] - nums[j - 1]);
+				answer = Math.min(answer, min - origin + removed);
+			}
+
+			System.out.println(answer);
+		}
+	}
 }
