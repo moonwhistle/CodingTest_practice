9c347237962dad3fc604ea7ba3d0199bf175b0a7
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-26 11:14:35 +0900

    [level 1] Title: 최소직사각형, Time: 1.92 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,16 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-
-        //가로 세로
-        int x = 0;
-        int y = 0;
+        int row = 0;
+        int col = 0;
 
         for (int[] size : sizes) {
             int max = Math.max(size[0], size[1]);
             int min = Math.min(size[0], size[1]);
 
-            if (max > x) {
-                x = max;
-            }
-            if (min > y) {
-                y = min;
-            }
+            row = Math.max(max, row);
+            col = Math.max(min, col);
         }
 
-        answer = x * y;
-        return answer;
+        return row * col;
     }
 }
