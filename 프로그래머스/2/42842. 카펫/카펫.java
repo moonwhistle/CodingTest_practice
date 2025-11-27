a9c91ba97a3a6a77cc5299c339ed9b81a0ce40c9
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-11-27 14:35:30 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,24 +1,23 @@
 class Solution {
-
-    public static int[] solution(int brown, int yellow) {
+    public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
 
-        for(int i = 1; i<=Math.sqrt(yellow); i++) {
-            if(yellow % i == 0) {
-                int y = yellow / i;
-                if(canMakeSquare(i, y, brown)) {
-                    answer[0] = Math.max(i, y) + 2;
-                    answer[1] = Math.min(i, y) + 2;
-                }
+        for(int i = 1; i<= yellow; i++) {
+            if(yellow % i != 0) {
+                continue;
+            }
+
+            int col = i;
+            int row = yellow / i;
+            int squareCount = (col + 2) * 2 + row * 2;
+
+            if(squareCount == brown) {
+                answer[0] = row + 2;
+                answer[1] = col + 2;
+                break;
             }
         }
-        
-        return answer;
-    }
 
-    private static boolean canMakeSquare(int x, int y, int brown) {
-        int row = x * 2;
-        int colum = (y + 2) * 2;
-        return brown == (row + colum);
+        return answer;
     }
 }
