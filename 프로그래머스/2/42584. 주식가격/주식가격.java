a388dc9382c41044d0a80e051a005a2b3632a19d
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-04 13:21:20 +0900

    [level 2] Title: 주식가격, Time: 107.73 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,33 +3,56 @@ import java.util.Stack;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Point> st = new Stack<>();
+        Stack<PriceSecond> st = new Stack<>();
 
         for (int i = 0; i < prices.length; i++) {
-            while (!st.isEmpty() && st.peek().price > prices[i]) {
-                int idx = st.pop().idx;
-                answer[idx] = i - idx;
+
+            for (PriceSecond p : st) {
+                p.increase();
             }
 
-            st.add(new Point(prices[i], i));
+            while (!st.isEmpty() && st.peek().price() > prices[i]) {
+                PriceSecond p = st.pop();
+                answer[p.idx()] = p.second();
+            }
+
+            st.add(new PriceSecond(prices[i], i));
         }
 
-        while (!st.isEmpty()) {
-            int idx = st.pop().idx;
-            answer[idx] = prices.length - 1 - idx;
+        for (PriceSecond p : st) {
+            answer[p.idx()] = p.second();
         }
 
         return answer;
     }
 }
 
-class Point {
+class PriceSecond {
 
-    int price;
-    int idx;
+    private final int price;
+    private final int idx;
+    private int second;
 
-    public Point(int price, int idx) {
+
+    PriceSecond(int price, int idx) {
         this.price = price;
         this.idx = idx;
+        this.second = 0;
+    }
+
+    public int idx() {
+        return this.idx;
+    }
+
+    public void increase() {
+        this.second++;
+    }
+
+    public int price() {
+        return this.price;
+    }
+
+    public int second() {
+        return this.second;
     }
 }
