5c44c372054e724b5e1931bc71d2c3ef73157deb
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-08 15:07:45 +0900

    [level 3] Title: 네트워크, Time: 3.14 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,47 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 class Solution {
 
+    static ArrayList<Integer>[] map;
     static boolean[] visited;
-
-    public static void main(String[] args) {
-        solution(3, new int[][]{{1, 1, 0}, {1, 0, 1}});
-    }
 
     public static int solution(int n, int[][] computers) {
         int answer = 0;
+        map = new ArrayList[n];
         visited = new boolean[n];
 
-        for(int i = 0; i< computers.length; i++) {
-            if(!visited[i]) {
-                answer++;
-                dfs(computers, i);
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = 0; j < computers[i].length; j++) {
+                if (j == i) {
+                    continue;
+                }
+
+                if (computers[i][j] == 1) {
+                    map[i].add(j);
+                }
             }
         }
-        
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i);
+            }
+        }
+
         return answer;
     }
 
-    private static void dfs(int[][] computers, int i) {
-        visited[i] = true;
+    private static void dfs(int start) {
+        visited[start] = true;
 
-        for(int j = 0 ;j <computers.length; j++) {
-            if(computers[i][j] == 1 && !visited[j] ) {
-                dfs(computers, j);
+        for (int element : map[start]) {
+            if (!visited[element]) {
+                dfs(element);
             }
         }
     }
