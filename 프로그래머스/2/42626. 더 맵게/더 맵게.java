68850157c116a40e8e70b461839cd7a369a7faa3
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-11 16:54:31 +0900

    [level 2] Title: 더 맵게, Time: 1672.99 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,23 +3,34 @@ import java.util.PriorityQueue;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = setPq(scoville);
 
-        for (int scov : scoville) {
-            pq.add(scov);
-        }
+        while (pq.size() > 1) {
+            if (pq.peek() >= K) {
+                return answer;
+            }
 
-        while (pq.peek() < K && pq.size() >= 2) {
             int first = pq.poll();
             int second = pq.poll();
-            pq.add(first + second * 2);
+
+            pq.add(first + (second * 2));
             answer++;
         }
 
-        if (pq.peek() < K) {
-            answer = -1;
+        if (pq.peek() > K) {
+            return answer;
         }
 
-        return answer;
+        return -1;
+    }
+
+    private PriorityQueue<Integer> setPq(int[] scoville) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int sc : scoville) {
+            pq.add(sc);
+        }
+
+        return pq;
     }
 }
