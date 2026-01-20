c2a64656c011a21bbf43b990f1539f1b6aa70c0c
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-01-20 17:46:06 +0900

    [level 1] Title: 달리기 경주, Time: 281.32 ms, Memory: 369 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -3,28 +3,25 @@ import java.util.Map;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-
-        Map<String, Integer> rank = new HashMap<>();
-
-        for (int i = 0; i < players.length; i++) {
-            rank.put(players[i], i);
-        }
-
-        for (String call : callings) {
-            int ranking = rank.get(call);
-
-            String prev = players[ranking - 1];
-            String current = players[ranking];
-
-            players[ranking] = prev;
-            players[ranking - 1] = current;
-
-            rank.put(prev, ranking);
-            rank.put(current, ranking - 1);
+        Map<String, Integer> ranks = new HashMap<>();
+        
+        for(int i = 0 ; i < players.length ; i ++) {
+            ranks.put(players[i], i);
         }
         
-        answer = players;
-        return answer;
+        for(String call : callings) {
+            int nowRank = ranks.get(call);
+            
+            String current = players[nowRank];
+            String prev = players[nowRank - 1];
+            
+            players[nowRank - 1] = current;
+            players[nowRank] = prev;
+            
+            ranks.put(current, nowRank - 1);
+            ranks.put(prev, nowRank);
+        }
+        
+        return players;
     }
 }
