756b876370221e6b256d6722cd99257a9d74ae50
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-02-23 22:42:21 +0900

    Title: 겹치지 않게 선분 고르기, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

--- 260223/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 260223/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-    
+
     static int cnt;
-    static int[] end;
+    static int[] nums;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,22 +14,37 @@ public class Main {
             segments[i][1] = sc.nextInt();
         }
         // Please write your code here.
-        end = new int[1001];
+        nums = new int[1001];
         cnt = 0;
         btk(n, 0, segments, 0);
         System.out.println(cnt);
     }
 
     private static void btk(int n, int idx, int[][] segments, int count) {
+        cnt = Math.max(cnt, count);
+
         for(int i = idx ; i < n ; i++) {
             int[] segment = segments[i];
+            int s = segment[0];
+            int e = segment[1];
+            boolean notContain = true;
 
-            if(end[segment[1]] != 1) {
+            for(int j = s; j <= e; j++) {
+                if(nums[j] > 0) {
+                    notContain = false;
+                    break;
+                }
+            }
+
+            if(notContain) {
                 count++;
-                end[segment[1]]++;
-                cnt = Math.max(cnt, count);
+                for(int j = s; j <= e; j++) {
+                    nums[j]++;
+                }
                 btk(n, i + 1, segments, count);
-                end[segment[1]]--;
+                for(int j = s; j <= e; j++) {
+                    nums[j]--;
+                }
                 count--;
             }
         }
