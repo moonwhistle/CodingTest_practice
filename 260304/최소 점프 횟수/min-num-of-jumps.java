cebc13a88c67e673ac17b037c52801df79090caa
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-03-04 09:19:30 +0900

    Title: 최소 점프 횟수, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 260304/최소 점프 횟수/min-num-of-jumps.java
+++ 260304/최소 점프 횟수/min-num-of-jumps.java
@@ -4,37 +4,41 @@ public class Main {
 
     static int n;
     static int[] arr;
-    static int[] dp;
+    static int min;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         arr = new int[n];
-        dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        min = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         // Please write your code here.
-        dp();
-        
-        if(dp[n-1] == Integer.MAX_VALUE) {
+        btk(0, 0);
+
+        if(min == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
-            System.out.println(dp[n-1]);
+            System.out.println(min);
         }
     }
 
-    private static void dp() {
-        dp[0] = 0;
+    private static void btk(int idx, int count) {
+        if(idx == n-1) {
+            min = Math.min(min, count);
+            return;
+        }
 
-        for(int i = 0 ; i < n ; i++) {
-            int now = dp[i];
+        if(idx >= n) {
+            return;
+        }
 
-            for(int j = i + 1; j < Math.min(i + 1 + arr[i], n); j++) {
-                dp[j] = Math.min(dp[j], now + 1);
-            }
+        int plus = arr[idx];
+
+        for(int i = idx + 1; i < idx + plus + 1; i++) {
+            btk(i, count + 1);
         }
     }
 }
