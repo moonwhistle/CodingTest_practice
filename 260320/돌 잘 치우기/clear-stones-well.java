a2c9a96947aadc1f1adcd9b5002525bf22717b90
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-03-20 12:01:26 +0900

    Title: 돌 잘 치우기, Time: 1409ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 260320/돌 잘 치우기/clear-stones-well.java
+++ 260320/돌 잘 치우기/clear-stones-well.java
@@ -14,25 +14,30 @@ class Main {
     static int[][] map;
     static boolean[][] visited;
     static List<int[]> starts;
+    static List<int[]> stones;
     static int max;
     static int[] dy = {0, 1, 0, -1};
     static int[] dx = {1, 0, -1, 0};
-    
+
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        visited = new boolean[N][N];
         starts = new ArrayList<>();
         max = Integer.MIN_VALUE;
-        
+        stones = new ArrayList<>();
+
         for(int y = 0 ; y < N ; y++) {
             st = new StringTokenizer(br.readLine());
 
             for(int x = 0 ; x < N ; x++) {
                 map[y][x] = Integer.parseInt(st.nextToken());
+
+                if(map[y][x] == 1) {
+                    stones.add(new int[]{y, x});
+                }
             }
         }
 
@@ -44,36 +49,54 @@ class Main {
         }
 
         // do
-        for(int[] start : starts) {
-            int y = start[0];
-            int x = start[1];
-            visited[y][x] = true;
-            btk(y, x, 0, 0);
-            visited[y][x] = false;
-        }
-
-        System.out.println(max);
+        removeStone(0, 0);
+        System.out.println(max + starts.size());
     }
 
-    private static void btk(int y, int x, int removeCnt, int moveCnt) {
-        if(map[y][x] == 1 && removeCnt >= M) {
-            max = Math.max(max, moveCnt);
+    private static void removeStone(int count, int idx) {
+        if(count == M) {
+            bfs();
             return;
         }
 
-        if(map[y][x] == 1) {
-            removeCnt++;
-        }
-
-        for(int i = 0 ; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if(ny >= 0 && nx >=0 && ny < N && nx < N && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                btk(ny, nx, removeCnt, moveCnt + 1);
-                visited[ny][nx] = false;
-            }
+        for(int i = idx; i < stones.size(); i++) {
+            int[] p = stones.get(i);
+            int y = p[0];
+            int x = p[1];
+            map[y][x] = 0;
+            removeStone(count + 1, idx + 1);
+            map[y][x] = 1;
         }
     }
+
+    private static void bfs() {
+        int count = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        visited = new boolean[N][N];
+
+        for(int[] start : starts) {
+            queue.add(start);
+            visited[start[0]][start[1]] = true;
+        }
+
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int y = now[0];
+            int x = now[1];
+
+            for(int i = 0 ; i < 4 ; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if(ny >=0 && nx >=0 && ny < N && nx < N && !visited[ny][nx] && map[ny][nx] == 0) {
+                    visited[ny][nx] = true;
+                    count++;
+                    queue.add(new int[]{ny, nx});
+                }
+            }
+        }
+
+        max = Math.max(max, count);
+    }
+
 }
