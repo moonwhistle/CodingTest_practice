3845be0ca139de952d43c047f0175ebb140b4942
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-04-15 17:23:24 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,40 +1,40 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-
-    public static void main(String[] args) {
-        solution(5, new int[]{2, 4}, new int[]{1, 3, 5});
-    }
-
-    public static int solution(int n, int[] lost, int[] reserve) {
+    public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] students = new int[n + 2];
-        Arrays.fill(students, 1);
-
-        for (int l : lost) {
-            students[l]--;
+        int[] count = new int[n + 1];
+        Arrays.fill(count, 1);
+        
+        for(int e : lost) {
+            count[e]--;
         }
-
-        for (int r : reserve) {
-            students[r]++;
+        
+        for(int e : reserve) {
+            count[e]++;
         }
-
-        for (int i = 1; i < n + 1; i++) {
-            if (students[i] > 1 && students[i - 1] < 1) {
-                students[i]--;
-                students[i - 1]++;
-            } else if (students[i] > 1 && students[i + 1] < 1) {
-                students[i]--;
-                students[i + 1]++;
+        
+        for(int i = 1; i <= n; i++) {
+            if(count[i] < 1) {
+                if(i - 1 >= 1 && count[i - 1] > 1) {
+                    count[i-1]--;
+                    count[i]++;
+                    continue;
+                }
+                
+                if(i + 1 <= n && count[i + 1] > 1) {
+                    count[i + 1]--;
+                    count[i]++;
+                }
             }
         }
-
-        for (int student : students) {
-            if (student >= 1) {
+        
+        for(int i = 1; i <= n; i++) {
+            if(count[i] > 0) {
                 answer++;
             }
         }
-
-        return answer - 2;
+        
+        return answer;
     }
 }
