e483a272170b7106e50c3fc709cd5fad4de3686c
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-04-30 01:08:21 +0900

    [level 2] Title: 숫자 변환하기, Time: 374.89 ms, Memory: 262 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,44 +1,51 @@
 import java.util.*;
 
 class Solution {
+    
+    static int answer;
+    
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        int[] dp = new int[y + 1];
-        Arrays.fill(dp, -1);
-        dp[x] = 0;
+        answer = Integer.MAX_VALUE;
+        bfs(x, y, n);
         
-        for(int i = x + 1; i < y + 1; i++) {
-            int a = Integer.MAX_VALUE;
-            int b = Integer.MAX_VALUE;
-            int c = Integer.MAX_VALUE;
-            
-            if(i - n > 0 && dp[i-n] > -1) {
-                a = dp[i -n];
-            }
-            
-            if(i % 2 == 0) {
-                if(i / 2 > -1 && dp[i / 2] > -1) {
-                    b = dp[i / 2];
-                }
-            } 
-            
-            if(i % 3 == 0) {
-                if(i / 3 > -1 && dp[i / 3] > -1) {
-                c = dp[i / 3];
-                }
-            }
-            
-            
-            
-            if(a == Integer.MAX_VALUE && b == Integer.MAX_VALUE && c == Integer.MAX_VALUE ) {
+        if(answer == Integer.MAX_VALUE) {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+    
+    private void bfs(int x, int y, int n) {
+        Queue<int[]> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(new int[]{x, 0});
+        
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int num = now[0];
+            int cnt = now[1];
+        
+            if(num == y) {
+                answer = Math.min(cnt, answer);
                 continue;
             }
             
-            dp[i] = Math.min(a, Math.min(b, c)) + 1;
+            if(num > y) {
+                continue;
+            }
+            
+            int[] nextValues = {
+                num + n,
+                num * 2,
+                num * 3
+            };
+
+            for (int next : nextValues) {
+                if (next <= y && !visited.contains(next)) {
+                    visited.add(next);
+                    queue.offer(new int[]{next, cnt + 1});
+                }
+            }
         }
-        
-        answer = dp[y];
-        
-        return answer;
     }
 }
