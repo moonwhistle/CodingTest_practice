5c36fa8c22c732fe4872d8287c4a87cab60349b7
Orange banana <106858113+moonwhistle@users.noreply.github.com>
2026-07-17 18:08:20 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,52 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-
-    static ArrayList<Integer>[] map;
-    static boolean[] visited;
-
-    public static int solution(int n, int[][] computers) {
+    
+    static int[] node;
+    
+    public int solution(int n, int[][] computers) {
         int answer = 0;
-        map = new ArrayList[n];
-        visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            map[i] = new ArrayList<>();
+        node = new int[n];
+        
+        for(int i = 0 ; i < node.length; i++) {
+            node[i] = i;
         }
-
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
-                if (j == i) {
+        
+        for(int i = 0 ; i < computers.length; i++) {
+            for(int j = 0 ; j < computers[i].length; j++) {
+                if(i == j) {
                     continue;
-                }
-
-                if (computers[i][j] == 1) {
-                    map[i].add(j);
+                } else if(computers[i][j] == 1) {
+                    union(i, j);
                 }
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                answer++;
-                dfs(i);
-            }
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0 ; i < node.length; i++) {
+            set.add(find(node[i]));
         }
-
-        return answer;
+        
+        return set.size();
     }
-
-    private static void dfs(int start) {
-        visited[start] = true;
-
-        for (int element : map[start]) {
-            if (!visited[element]) {
-                dfs(element);
-            }
+    
+    private void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        
+        if(rootA == rootB) {
+            return;
         }
+        
+        node[rootB] = rootA;
+    }
+    
+    private int find(int i) {
+        if(node[i] == i) {
+            return i;
+        }
+        
+        return node[i] = find(node[i]);
     }
 }
